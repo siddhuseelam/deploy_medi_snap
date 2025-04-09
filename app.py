1e2cfe854a5e9cfd6f10fd5cd78c50c6e23ddcf0
siddhuseelam <siddhuseelam@gmail.com>
2025-04-10 02:16:51 +0530

    changed flask app

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def serve_react_app(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
